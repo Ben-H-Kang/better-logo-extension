9ff27ac44c4fa2083864cc7a9179c87917ef2b50
Ben-H-Kang <134357632+Ben-H-Kang@users.noreply.github.com>
2023-08-05 15:08:04 -0700

    initial commit

---
+++ contentScript.js
@@ -0,0 +1,28 @@
+// alert("contentScript entrance");
+setTimeout(realLogoSwap, 2000);
+
+function realLogoSwap() {
+    const fakeLogo = document.querySelector(".css-56i8za");
+    const realLogo = document.createElement('img');
+    realLogo.src = '#.png';
+    realLogo.alt = 'the real refer me logo';     
+    realLogo.style.cssText = "height: 32px; width: 32px"
+    const logoHolder = document.querySelector('.chakra-stack.css-1igwmid');
+    logoHolder.replaceChild(realLogo, fakeLogo)
+}
+
+const observer = new MutationObserver(mutations => {
+    for (const mutation of mutations) {
+        if (mutation.type === 'childList') {
+            realLogoSwap();
+        }
+    }
+});
+
+function startObserving() {
+    const targetNode = document.querySelector('body');
+    const config = { childList: true, subtree: true };
+    observer.observe(targetNode, config);
+}
+
+startObserving();
