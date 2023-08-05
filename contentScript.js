b0ecafc6f068915f8053e616faefffe9385bcde7
Ben-H-Kang <134357632+Ben-H-Kang@users.noreply.github.com>
2023-08-05 15:09:16 -0700

    Add files via upload

--- contentScript.js
+++ contentScript.js
@@ -4,7 +4,7 @@ setTimeout(realLogoSwap, 2000);
 function realLogoSwap() {
     const fakeLogo = document.querySelector(".css-56i8za");
     const realLogo = document.createElement('img');
-    realLogo.src = '#.png';
+    realLogo.src = 'https://raw.githubusercontent.com/Ben-H-Kang/better-logo-extension/main/Safeimagekit-resized-img.png';
     realLogo.alt = 'the real refer me logo';     
     realLogo.style.cssText = "height: 32px; width: 32px"
     const logoHolder = document.querySelector('.chakra-stack.css-1igwmid');
